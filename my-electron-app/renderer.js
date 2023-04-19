eb20cc163bb642dc798c45934f53e0c4409da953
northboat <northboat@163.com>
2023-04-20 00:54:25 +0800

    initial

---
+++ my-electron-app/renderer.js
@@ -0,0 +1,20 @@
+// const information = document.getElementById('info');
+// information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+
+const func = async () => {
+  const response = await window.versions.ping()
+  alert(response) // 打印 'pong'
+}
+
+func()
+
+const render_send = async () => {
+  const data = { name: 'John', age: 30 };
+  const jsonData = JSON.stringify(data);
+  const response = await window.versions.send(jsonData)
+  alert(response)
+}
+
+
+const btn = document.getElementById('test');
+btn.onclick = render_send
